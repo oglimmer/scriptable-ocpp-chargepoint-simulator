aafdfd2a103eec085443cfe83c9074dde5864eb8
Oli Zimpasser <oglimmer@gmail.com>
2020-10-05 15:05:20 +0200

    safe-guard JSON.stringify for objects with circular references

--- src/log.ts
+++ src/log.ts
@@ -1,11 +1,31 @@
 import Debug from 'debug';
 import {logger} from './http-post-logger';
 
+function refReplacer() {
+  let m = new Map(), v= new Map(), init = null;
+
+  return function(field, value) {
+    let p= m.get(this) + (Array.isArray(this) ? `[${field}]` : '.' + field);
+    let isComplex= value===Object(value)
+
+    if (isComplex) m.set(value, p);
+
+    let pp = v.get(value)||'';
+    let path = p.replace(/undefined\.\.?/,'');
+    let val = pp ? `#REF:${pp[0]=='[' ? '$':'$.'}${pp}` : value;
+
+    !init ? (init=value) : (val===init ? val="#REF:$" : 0);
+    if(!pp && isComplex) v.set(value, path);
+
+    return val;
+  }
+}
+
 const format = (log: string | object): string => {
   if (typeof log === 'string') {
     return log;
   } else {
-    return JSON.stringify(log);
+    return JSON.stringify(log, refReplacer());
   }
 }
 
